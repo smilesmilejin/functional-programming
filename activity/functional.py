d3fabc6a8c527101ad4bd31993a3a65f80682efa
Xinshuang Jin <jinxinshuang@gmail.com>
2025-05-08 14:15:16 -0400

    completed all waves

--- activity/functional.py
+++ activity/functional.py
@@ -2,15 +2,17 @@
 # Write a function that takes in a list of words and returns the shortest
 # one. (assume no ties)
 
-def shortest_word(words):
-    pass
+# def shortest_word(words):
+#     # pass
+#     return min(words, key=len)
 
 # SUPER CHALLENGE: use functools.reduce
 # Only try this after completing the rest of the activity
 # You will need to research the use of the functools.reduce function
-# def shortest_word(words):
-#     from functools import reduce
-#     pass    
+def shortest_word(words):
+    from functools import reduce
+    # return reduce(lambda a, b: a if len(a) < len(b) else b, words)
+    return reduce(lambda shortest, word: word if len(word) < len(shortest) else shortest, words)   
 
 
 # Wave 2
@@ -19,7 +21,7 @@ def shortest_word(words):
 # Hint: remember to convert back to a list!
 
 def even_nums(nums):
-    pass
+    return list(filter(lambda num: num % 2 == 0, nums))
 
 # Wave 3
 # Write a function that takes in a list of numbers and returns a new list
@@ -27,7 +29,7 @@ def even_nums(nums):
 # Hint: remember to convert back to a list!
 
 def squares(nums):
-    pass
+    return list(map(lambda num: num ** 2, nums))
 
 # Wave 4
 # Write a function that accepts a word, a function, and the name of that 
@@ -35,7 +37,7 @@ def squares(nums):
 # "The result of applying FUNCTION_NAME to WORD is RESULT"
 
 def report(word, function, function_name):
-    pass
+    return f"The result of applying {function_name} to {word} is {function(word)}"
 
 # Wave 5
 # Write a function that takes a list of passwords and returns a list of only 
@@ -43,5 +45,6 @@ def report(word, function, function_name):
 # The returned list should be sorted by in order of increasing length.
 
 def sorted_valid_passwords(passwords):
-    pass
-
+    # pass
+    passwords_list = list(filter(lambda password: not password.isalpha(), passwords))
+    return sorted(passwords_list, key=len)
